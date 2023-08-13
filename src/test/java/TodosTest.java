28a7df95c5f8a067e8e89181211c576053514eca
Anastasiya M <mega.gara11@gmail.com>
2023-08-13 10:46:52 +0700

    src/

---
+++ src/test/java/TodosTest.java
@@ -0,0 +1,73 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class TodosTest {
+    @Test
+    public void shouldAddThreeTasksOfDifferentType() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+
+        String[] subtasks = { "Молоко", "Яйца", "Хлеб" };
+        Epic epic = new Epic(55, subtasks);
+
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask);
+        todos.add(epic);
+        todos.add(meeting);
+
+        Task[] expected = { simpleTask, epic, meeting };
+        Task[] actual = todos.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+  @Test
+    public void testOne(){
+      SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+      boolean expected = true;
+      boolean actual = simpleTask.matches("Позвонить");
+      Assertions.assertEquals(expected, actual);
+  }
+  @Test
+    public void testTwo(){
+      SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+      boolean expected = false;
+      boolean actual = simpleTask.matches("позвонить");
+      Assertions.assertEquals(expected, actual);
+  }
+  @Test
+    public void testThree(){
+        Task meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        boolean expected = true;
+        boolean actual = meeting.matches("вторник");
+
+  }
+
+  @Test
+    public void testFour(){
+        Task meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        boolean expected = false;
+        boolean actual = meeting.matches("во");
+  }
+
+  @Test
+    public void testFive(){
+        Task task = new Task(55);
+        boolean expected = false;
+        boolean actual = task.matches("родителям");
+  }
+}
